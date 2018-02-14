254607c709ff837335ad41848f94248dee80fceb
csx1_team <cmazzoli@bnl.gov>
2018-02-14 17:11:00 -0500

    updated hklsim to suppress warnings/errrors

--- profile_simulated/startup/00-nsls2-tools.py
+++ profile_simulated/startup/00-nsls2-tools.py
@@ -1,5 +1,9 @@
 # Modified in order to create a safe simulation environment 2018/01/30
 
+# added to suppress hkl import warning
+import gi
+gi.require_version("Hkl", "5.0")
+
 from csx1.startup import *
 
 #from IPython import get_ipython
